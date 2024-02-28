25d880bec22dfd2f4790f4af1814ae8b8fc4c040
Walid Kabboussa <walidkabou@gmail.com>
2024-02-28 13:57:58 +0100

    Setup Routers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,30 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+import SideBar from './components/SideBar'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
+import ExplorePage from './pages/ExplorePage'
+import HomePage from './pages/HomePage'
+import LikesPage from './pages/LikesPage'
+
 function App() {
 
 
+
   return (
-    <>
-  
-    </>
+   <div className='flex'>
+      <SideBar />
+      <div>
+        <Routes>
+          <Route path= '/' element = {<HomePage/>} />
+          <Route path= '/login' element = {<LoginPage/>} />
+          <Route path= '/signuo' element = {<SignUpPage/>} />
+          <Route path= '/explore' element = {<ExplorePage/>} />
+          <Route path= '/likes' element = {<LikesPage/>} />
+        </Routes>
+      </div>
+
+   </div>
   )
 }
 
---
+++ frontend/src/pages/LikesPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LikesPage = () => {
+  return (
+    <div className='text-red-400'> LikesPage</div>
+  )
+}
+
+export default LikesPage
