e7d325a5d6038e51cc8f04cb681bc9b21f9450fb
Walid Kabboussa <walidkabou@gmail.com>
2024-03-02 20:30:32 +0100

    fetch data from github api

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,41 @@
+import { useCallback, useEffect, useState } from "react"
 import ProfileInfo from "../components/ProfileInfo"
 import Repos from "../components/Repos"
 import Search from "../components/Search"
 import SortRepos from "../components/SortRepos"
+import toast from 'react-hot-toast';
 
 
 const HomePage = () => {
+  const [userProfile, setUserProfile] = useState(null)
+  const [repos, setRepos] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [sortType, setSortType] = useState('forks')
+
+
+  const getUserProfileAndRepos = useCallback(
+    async () => {
+      setLoading(true)
+        try {
+          const userRes = await fetch('https://api.github.com/users/hisoka37')
+          const userProfile = await userRes.json()
+          setUserProfile(userProfile)
+  
+          const userRepos = await fetch(userProfile.repos_url)
+          const repos = await userRepos.json()
+          setRepos(repos)
+     
+        } catch (error) {
+          toast.error(error.message)
+        } finally {
+          setLoading(false)
+        }
+  }, [])
+
+  useEffect(() => {
+      getUserProfileAndRepos()
+  }, [getUserProfileAndRepos])
+
   return (
      <div className="m-4">
       <Search />
