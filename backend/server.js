3ab2f49b3f26fabb5acda38ea84b68f34c3dc507
Walid Kabboussa <walidkabou@gmail.com>
2024-03-17 04:30:59 +0000

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import userRoutes from './routes/User.route.js';
+import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import exploreRouter from './routes/explore.route.js';
 import dotenv from "dotenv"
